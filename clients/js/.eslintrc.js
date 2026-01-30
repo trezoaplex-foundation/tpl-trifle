cb1b86ebb7c93c92c9212ed7b78c57496924e35d
trezoa-xyz <maintainers@trezoa.xyz>
2026-01-30 14:36:24 -0500

    chore: rebrand organization from trezoa-team to trezoa-team
    - Replace all trezoa-team references with trezoa-team
    - Replace all trezoateam references with trezoateam
    - Update GitHub URLs, Docker images, and documentation links
    - Update email addresses and package metadata

--- clients/js/.eslintrc.js
+++ clients/js/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: 'tsconfig.json',
+    trezoa: 'tsconfig.json',
     tsconfigRootDir: __dirname,
   },
   rules: {
